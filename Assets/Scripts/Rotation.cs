84c33d70d9a9253abafe8058471c5e8bfa46c970
Phi Nhung <ngnhung44@gmail.com>
2018-03-10 23:07:58 +0100

    Jupiter Rotation

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Rotation : MonoBehaviour {
+
+
+	public GameObject Bahn;
+	public float speed;
+	public GameObject DropZone;
+
+	void OrbitAround(){
+
+		transform.RotateAround (Bahn.transform.position, Vector3.up, speed * Time.deltaTime); //Rotieren des Planeten
+	}
+
+	bool isSnapped;
+
+	void Update () {
+
+		GameObject snap = GameObject.Find ("JupiterSnapZone");
+		isSnapped = snap.GetComponent<VRTK_SnapDropZone> ().isSnapped; //isSnapped aus dem Script der SnapDropZone
+		if (isSnapped == true) {
+			OrbitAround (); //rotieren, wenn Planet an richtiger Stelle
+
+		}
+	}
+}
