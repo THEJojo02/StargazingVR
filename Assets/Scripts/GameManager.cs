c6c9d8c164bd9c478514b162556ab939d0047c7e
Unknown <bergerjohanna@t-online.de>
2018-05-05 14:39:05 +0200

    Countdown

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     public float time = 0 ;
+    public TextMesh textObject = GameObject.Find("").GetComponent<TextMesh>();
+    float min = 0;
+    float sek = 0;
+    float x;
+    float y;
 
     public void LoadScenea()
 	{
@@ -15,11 +20,18 @@ public class GameManager : MonoBehaviour {
 		LoadScenea ();
 	}
 
-   void FixedUpdate()
+    void FixedUpdate()
     {
+        min = Mathf.Floor(time / 60);
+        sek = Mathf.Round(60 - (time % 60));
+        x = 14 - min;
+        if (sek < 10)
+        { textObject.text = x.ToString() + ":0" + sek.ToString(); }
+        else
+        { textObject.text = x.ToString() + ":" + sek.ToString(); }
         if (time > 900)
         {
-         SceneManager.LoadScene("Lose", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Lose", LoadSceneMode.Additive);
         }
         time = (Time.deltaTime + time);
     }
